4f3116d10f0eefa7a6af5228dc3fb75e60a230ca
Rhythm Shandlya <armaanbgp@gmail.com>
2023-05-14 18:56:51 +0530

    get swagger

--- src/routes/v1/swagger.route.ts
+++ src/routes/v1/swagger.route.ts
@@ -1,19 +1,20 @@
-import express from 'express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-
-import swaggerDefinition from '../../modules/swagger/swagger.definition';
+import config from "../../config/config";
+import express from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { getSwagger } from "@dinedrop/shared";
 
 const router = express.Router();
+const swaggerDefinition = getSwagger(config.port);
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ['packages/components.yaml', 'dist/routes/v1/*.js'],
+  apis: ["packages/components.yaml", "dist/routes/v1/*.js"],
 });
 
-router.use('/', swaggerUi.serve);
+router.use("/", swaggerUi.serve);
 router.get(
-  '/',
+  "/",
   swaggerUi.setup(specs, {
     explorer: true,
   })
